1e57f37468f49260f6202aae0c6dd05f45eeac5e
bogdansemkin <donbod8@gmail.com>
2021-11-16 12:15:43 +0200

    Update gpstools.go

--- gpstools.go
+++ gpstools.go
@@ -369,7 +369,6 @@ func updateGBRstatus(update_GBR_id, update_CMD, update_GEO string, update_GBR_st
 					gbrAlarmCanceled[i] = 0
 					gbrAlarmLast[i] = ""
 					gbrAlarmCard[i] = ""
-
 				}
 			}
 		} else if update_GBR_status == 2 {
